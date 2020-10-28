f1fd661ae5adb0448fdf32c0c60f4b94306ce4ef
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-10-28 18:58:10 -0400

    Fixes to prealloc size (breaking on vector case)

--- sphere_distance.m
+++ sphere_distance.m
@@ -2,8 +2,8 @@
 %Computes the signed distance between points and the sphere, while taking into
 %account whether the sphere is hollow or filled in.
 function [dPointsSphere]=sphere_distance(sphere,points)
-    dPointsSphere = zeros(1, length(points));
-    for i=1:length(points)
+    dPointsSphere = zeros(1, size(points,2));
+    for i=1:size(points,2)
         dPCenter = sqrt((points(1,i)-sphere.xCenter(1))^2+(points(2,i)-sphere.xCenter(2))^2);
         dPointsSphere(i) = dPCenter - abs(sphere.radius);
         if (sphere.radius<0); dPointsSphere(i) = dPointsSphere(i)*(-1); end
--- sphere_distanceGrad.m
+++ sphere_distanceGrad.m
@@ -2,3 +2,14 @@
 %Computes the gradient of the signed distance between points and the sphere,
 %consistently with the definition of sphere_distance.
 function [gradDPointsSphere]=sphere_distanceGrad(sphere,points)
+    gradDPointsSphere = zeros(2, size(points,2));
+    for i=1:size(points,2)
+        if (sphere.xCenter == points(:,i))
+            gradDPointsSphere(:,i) = zeros(2,1);
+        else
+            vecPCenter = sphere.xCenter' - points(:,i);
+            dPCenter = sqrt((points(1,i)-sphere.xCenter(1))^2+(points(2,i)-sphere.xCenter(2))^2);
+            gradDPointsSphere(:,i) = vecPCenter/dPCenter;
+        end
+    end
+end
