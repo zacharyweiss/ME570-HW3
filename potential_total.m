0475acbf765fbce8f79e99105d1c9458ac75a47c
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-10-28 20:04:46 -0400

    Dist dimension fixes, total potential and gradient functions

--- potential_total.m
+++ potential_total.m
@@ -2,3 +2,10 @@
 %Compute the function $U=U_ attr+  @ @ _iU_ rep,i$, where $ $ is given by the
 %variable  @x   potential.repulsiveWeight
 function [U]=potential_total(xEval,world,potential)
+    potRepTot = 0;
+    for i=1:size(world,2)
+        potRepTot = potRepTot + potential_repulsiveSphere(xEval,world(i));
+    end
+    
+    U = potential_attractive(xEval,potential) + potential.repulsiveWeight*potRepTot;
+end
--- potential_totalGrad.m
+++ potential_totalGrad.m
@@ -2,3 +2,11 @@
 %Compute the gradient of the total potential, $ U= U_ attr+  @ @ _i U_ rep,i$,
 %where $ $ is given by the variable  @x   potential.repulsiveWeight
 function [gradU]=potential_totalGrad(xEval,world,potential)
+    potRepGradTot = zeros(size(xEval,1),1);
+    for i=1:size(world,2)
+        potRepGradTot = potRepGradTot + potential_repulsiveSphereGrad(xEval,world(i));
+    end
+    
+    gradU = potential_attractiveGrad(xEval,potential) + potential.repulsiveWeight*potRepGradTot;
+
+end
