fcdb04d92c849984b171b5724d1408b3323d8222
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-01 17:20:24 -0500

    Sign convention, style hinting

--- sphere_distanceGrad.m
+++ sphere_distanceGrad.m
@@ -3,13 +3,13 @@
 %consistently with the definition of sphere_distance.
 function [gradDPointsSphere]=sphere_distanceGrad(sphere,points)
     gradDPointsSphere = zeros(size(points));
-    for i=1:size(points,2)
-        if (sphere.xCenter == points(:,i))
-            gradDPointsSphere(:,i) = zeros(2,1);
+    for iPts=1:size(points,2)
+        if sphere.xCenter == points(:,iPts)
+            gradDPointsSphere(:,iPts) = zeros(2,1);
         else
-            vecPCenter = sphere.xCenter - points(:,i);
-            dPCenter = sqrt((points(1,i)-sphere.xCenter(1))^2+(points(2,i)-sphere.xCenter(2))^2);
-            gradDPointsSphere(:,i) = vecPCenter/dPCenter;
+            vecPCenter = sphere.xCenter - points(:,iPts);
+            dPCenter = sqrt((points(1,iPts)-sphere.xCenter(1))^2+(points(2,iPts)-sphere.xCenter(2))^2);
+            gradDPointsSphere(:,iPts) = vecPCenter/dPCenter;
         end
     end
 end
