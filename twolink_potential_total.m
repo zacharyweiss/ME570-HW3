ee0130af16323cb4a3b00af6ea5c09899ebe6a1b
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-02 01:46:54 -0500

    Theta pullbacks to R2

--- twolink_potential_total.m
+++ twolink_potential_total.m
@@ -4,3 +4,11 @@
 %q:total-potential, and $  Wp_ eff( )$ is the position of the end effector in the
 %world frame as a function of the joint angles $vec17E = _1\\ _2$.
 function [UTheta]=twolink_potential_total(thetaEval,world,potential)
+    twolinkPos = twolink_jacobianMatrix(thetaEval);
+    potRepTot = 0;
+    for iRep=1:size(world,2)
+        potRepTot = potRepTot + potential_repulsiveSphere(twolinkPos(:,1),world(iRep));
+    end
+    
+    UTheta = potential_attractive(twolinkPos(:,1),potential) + potential.repulsiveWeight*potRepTot;
+end
--- twolink_potential_totalGrad.m
+++ twolink_potential_totalGrad.m
@@ -2,3 +2,12 @@
 %Compute the gradient of the potential $U$ pulled back through the kinematic map
 %of the two-link manipulator, i.e., $ _vec17E  U(  Wp_ eff(vec17E ))$.
 function [gradUTheta]=twolink_potential_totalGrad(thetaEval,world,potential)
+    twolinkPos = twolink_jacobianMatrix(thetaEval);
+
+    potRepGradTot = zeros(size(thetaEval,1),1);
+    for i=1:size(world,2)
+        potRepGradTot = potRepGradTot + potential_repulsiveSphereGrad(twolinkPos(:,1),world(i));
+    end
+    
+    gradUTheta = potential_attractiveGrad(twolinkPos(:,1),potential) + potential.repulsiveWeight.*potRepGradTot;
+end
