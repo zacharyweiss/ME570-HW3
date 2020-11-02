d8ca9c7936fd178932bd0b9e40bcf62ca94fb102
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-02 01:34:56 -0500

    Jacobian equation fix

--- twolink_jacobianMatrix.m
+++ twolink_jacobianMatrix.m
@@ -6,7 +6,7 @@ function [Jtheta]=twolink_jacobianMatrix(theta)
     jOne = -5*sin(theta(1))-5*cos(theta(1))*sin(theta(2))-5*cos(theta(2))*sin(theta(1));
     jTwo = -5*cos(theta(1))*sin(theta(2))-5*cos(theta(2))*sin(theta(1));
     jThree = 5*cos(theta(1))+5*cos(theta(1))*cos(theta(2))-5*sin(theta(1))*sin(theta(2));
-    jFour = 5*cos(theta(1))*cos(theta(2))-5*sin(theta(1))*cos(theta(2))-5*sin(theta(1))*sin(theta(2));
+    jFour = 5*cos(theta(1))*cos(theta(2))-5*sin(theta(1))*sin(theta(2));
 
     Jtheta = [jOne jTwo;jThree jFour];
 end
